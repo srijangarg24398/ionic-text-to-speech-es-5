2eb75bafc503f051ef337cdde53d894271a45688
Srijan <srijan@localhost.localdomain>
2018-02-15 23:28:41 +0530

    text to speech

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { TextToSpeech } from '@ionic-native/text-to-speech';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	
+  text:string;
+  status:string="not reading";
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private tts: TextToSpeech) {
 
+  }
+
+  speakText(){
+	this.tts.speak(this.text)
+  	.then(() => {
+  		console.log("Successfully spoke "+this.text);
+  		status="Reading mode on";
+  	})
+  	.catch((reason: any) => console.log(reason));
   }
 
 }
